fc14b967f6b1d6c33f6779339f821940266c63d2
Ajay Gowda <omegacyll@gmail.com>
2024-01-23 19:16:13 -0700

    added iron nvim for python repl :)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -277,6 +277,9 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
+  {
+    'Vigemus/iron.nvim'
+  },
 
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
@@ -697,6 +700,63 @@ cmp.setup {
     { name = 'path' },
   },
 }
+
+local iron = require("iron.core")
+iron.setup {
+
+  config = {
+    -- Whether a repl should be discarded or not
+    scratch_repl = true,
+    -- Your repl definitions come here
+    repl_definition = {
+      sh = {
+        -- Can be a table or a function that
+        -- returns a table (see below)
+        command = { "zsh" }
+      }
+    },
+    -- How the repl window will be displayed
+    -- See below for more information
+    repl_open_cmd = require('iron.view').split.vertical.botright(0.45)
+  },
+  -- Iron doesn't set keymaps by default anymore.
+  -- You can set them here or manually add keymaps to the functions in iron.core
+  keymaps = {
+    send_motion = "<space>sc",
+    visual_send = "<space>sc",
+    send_file = "<space>sf",
+    send_line = "<space>sl",
+    send_until_cursor = "<space>su",
+    send_mark = "<space>sm",
+    mark_motion = "<space>mc",
+    mark_visual = "<space>mc",
+    remove_mark = "<space>md",
+    cr = "<space>s<cr>",
+    interrupt = "<space>s<space>",
+    exit = "<space>sq",
+    clear = "<space>cl",
+  },
+  -- If the highlight is on, you can change how it looks
+  -- For the available options, check nvim_set_hl
+  highlight = {
+    italic = true
+  },
+  ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+}
+
+-- iron also has a list of commands, see :h iron-commands for all available commands
+vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>')
+vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>')
+vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
+vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
+
+vim.keymap.set('n', '<space>j', '<C-w>j')
+vim.keymap.set('n', '<space>k', '<C-w>k')
+vim.keymap.set('n', '<space>h', '<C-w>h')
+vim.keymap.set('n', '<space>l', '<C-w>l')
+
+-- Terminal mode
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
